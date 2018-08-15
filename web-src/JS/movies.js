fcc528f1b6ef07810eae3238ccebbbf8178cf74f
Abrudean Sergiu <sergiu.abrudean@evozon.com>
2018-08-15 17:43:22 +0300

    fix

--- web-src/JS/movies.js
+++ web-src/JS/movies.js
@@ -1,21 +1,38 @@
-
-Query(document).ready(function(){
-//click on save changes button then Save via ajax
-    $("#saveChanges").click(function() {
-        $.ajax({
-            type: "POST",
-            url: "saveData.action",
-            data: "{}",
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function() {
-
-                msgBox("Saved Changes");
-            },
-            error: function(){
-                msgbox("Falied to save Changes");
-            }
-        });
-        return false;
+$(document).ready(function () {
+    $.ajax({
+        type: 'GET',
+        url: '/movies/all',
+        dataType: "json",
+        // async: false,
+        success: function (data) {
+            console.log(data);
+            var table = $("#table tbody");
+            $(table).empty();
+            $.each(data, function (idx, elem) {
+                table.append("<tr><td>" + elem.name +
+                    "</td><td>" + elem.year + "</td>   <td><img src='" +
+                    elem.image + "'></td>" +
+                    "<td>" + elem.genres + "</td></tr>");
+            });
+            return false;
+        },
+        error: function (data) {
+            alert(data);
+            return false;
+        }
     });
+    console.log('doc was loaded');
 });
+// $.holdReady(true);
+// $.get("http://www.cinema.local/movies/all", function (data) {
+//     var table = ('#table');
+//     $(table).empty();
+//     table = $("#table tbody");
+//     $.each(JSON.parse(data), function (idx, elem) {
+//         table.append("<tr><td>" + elem.name +
+//             "</td><td>" + elem.year + "</td>   <td><img src='" +
+//             elem.image + "'></td>" +
+//             "<td>" + elem.genres + "</td></tr>");
+//     });
+//     $.holdReady(false);
+// });
